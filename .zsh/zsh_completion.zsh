7d7a0936d97e815f4d554e22e0143dd37ee344f0
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2016-12-31 14:14:01 +0100

    zsh: tweak completion of autocd

--- .zsh/zsh_completion.zsh
+++ .zsh/zsh_completion.zsh
@@ -1,7 +1,7 @@
 if [ "$PS1" ]; then
   # complist section
-  zmodload -i zsh/complist 
-  
+  zmodload -i zsh/complist
+
   setopt autocd                  # change to dirs without cd
   setopt extendedglob            # weird & wacky pattern matching - yay zsh!
   setopt completeinword          # not just at the end
@@ -10,14 +10,17 @@ if [ "$PS1" ]; then
   setopt interactivecomments     # escape commands so i can use them later
   setopt printexitvalue          # alert me if something's failed
   setopt noshwordsplit           # use zsh style word splitting
-  
+
+  # autocd expanding
+  cdpath=(. $GOROOT/src $GOPATH/src/golang.org $GOPATH/src/github.com $GOPATH/src/github.com/szuecs)
+
   #
   # completion tweaking
   #
   autoload -U compinit; compinit
   zstyle ':completion:*' use-cache on
   zstyle ':completion:*' users resolve
-  # activate menu selection if >=5 possibilities 
+  # activate menu selection if >=5 possibilities
   zstyle ':completion:*' menu select=5
   # use dircolours in completion listings
   zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
@@ -31,16 +34,18 @@ if [ "$PS1" ]; then
   zstyle ':completion:*:warnings' format "%B$fg[red]%}---- no match for: $fg[white]%d%b"
   # let's use the tag name as group name
   zstyle ':completion:*' group-name ''
-  # activate approximate completion, but only after regular completion (_complete) 
+  # activate approximate completion, but only after regular completion (_complete)
   # and matching (_match)
   zstyle ':completion:*' completer _complete _match _approximate
   # limit to 2 errors
   zstyle ':completion:*:approximate:*' max-errors 2
   # let's complete known hosts and hosts from ssh's known_hosts file
   #basehost="host1.example.com host2.example.com"
-  hosts=($((
-  ( [ -r .ssh/known_hosts ] && awk '{print $1}' .ssh/known_hosts | tr , '\n');\
-  echo $basehost; ) | sort -u | grep -v \|1\| )) 
+  hosts=(
+    $(
+      (
+        ( [ -r .ssh/known_hosts ] && awk '{print $1}' .ssh/known_hosts | tr , '\n');
+        echo $basehost; ) | sort -u | grep -v \|1\| ))
   zstyle ':completion:*' hosts $hosts
 
   zstyle ':completion:*:vi:*' ignored-patterns '*.(o|a|so|dmg|dylib|aux|dvi|log|swp|fig|bbl|blg|bst|idx|ind|out|toc|class|pyc|rbc|dSYM)'
@@ -49,10 +54,10 @@ if [ "$PS1" ]; then
   zstyle ':completion:*:emacs:*' ignored-patterns '*.(o|a|so|dmg|dylib|aux|dvi|log|swp|fig|bbl|blg|bst|idx|ind|out|toc|class|pyc|rbc|dSYM)'
   zstyle ':completion:*:emacsclient:*' ignored-patterns '*.(o|a|so|dmg|dylib|aux|dvi|log|swp|fig|bbl|blg|bst|idx|ind|out|toc|class|pyc|rbc|dSYM)'
   zstyle ':completion:*:e:*' ignored-patterns '*.(o|a|so|dmg|dylib|aux|dvi|log|swp|fig|bbl|blg|bst|idx|ind|out|toc|class|pyc|rbc|dSYM)'
-  
+
   # source .zsh/Completion/*
   . ~/.zsh/Completion/*.zsh
-  
+
   # TODO: add auto git prompt
   # TODO: change colors to be usable on white background
   # TODO: ignore git repo if it is dotfiles.git
